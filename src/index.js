bdeb96587c63a3536133008dd355e06834810118
xiscoreal009-cpu <xiscoreal009@gmail.com>
2026-04-12 13:42:37 +0530

    util d=code discussion

--- src/index.js
+++ src/index.js
@@ -8,9 +8,17 @@ dotenv.config({
   path: './.env'
 })
 connectDB()
+.then(()=>{
+  app.listen(process.env.PORT || 8000,()=>{
+  console.log(`server started successfully${process.env.PORT}`)
 
+  })
+})
+.catch((error)=>{
+  console.log("connection error",error)
+})
 
-
+     
 
 
 /*
