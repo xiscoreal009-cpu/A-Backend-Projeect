6783ec5bddb5770a4ca863fb217375bd42766de8
xiscoreal009-cpu <xiscoreal009@gmail.com>
2026-04-14 19:39:56 +0530

    refresh token

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,6 +1,7 @@
 import mongoose, { Schema } from "mongoose";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
+import { use } from "react";
 const userSchema = new Schema(
   {
     username: {
@@ -56,4 +57,27 @@ userSchema.pre("save", function (next) {
 userSchema.methods.isPasswordCorrect = async function (password) {
   return await bcrypt.compare(password, this.password);
 };
+userSchema.methods.generateAccessToken=function(){
+  jwt.sign({
+    _id : this._id,
+    email: this.email,
+    username: this.username,
+    fullname: this.fullname,
+  },
+  process.env.ACCESS_TOKEN_SECRET,{
+    expiresIn : ACCESS_TOKEN_EXPIRY
+  }
+)
+}
+userSchema.methods.generateRefreshToken=function(){
+  jwt.sign({
+    _id : this._id,
+    email: this.email,
+    username: this.username,
+    fullname: this.fullname,
+  },
+  process.env.REFRESH_TOKEN_SECRET,{
+    expiresIn : REFRESH_TOKEN_EXPIRY
+  })
+}
 export const User = mongoose.model("User", userSchema);
